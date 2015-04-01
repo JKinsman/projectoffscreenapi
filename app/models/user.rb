5a4bf2895ab2cb611e8dfdcc5dfb2a328b42180e
Jake Kinsman <jkinsman1986@Jakes-MacBook-Pro.local>
2015-04-01 18:55:31 -0400

    Ooooops

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  #validates_uniqueness_of :username, case_sensitive: false
-  #validates_uniqueness_of :email, case_sensitive: false
+  validates_uniqueness_of :username, case_sensitive: false
+  validates_uniqueness_of :email, case_sensitive: false
 end
