74deaf03a83e1902f141724a40ce3e3ab4f36dc1
Jake Kinsman <jkinsman1986@Jakes-MacBook-Pro.local>
2015-04-09 19:36:58 -0400

    Changing User schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401150249) do
+ActiveRecord::Schema.define(version: 20150409233436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "password"
-    t.string   "username"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "username"
   end
 
   create_table "widgets", force: true do |t|
