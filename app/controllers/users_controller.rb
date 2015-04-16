60907bfdee7228af3cd6b61e01afbec54b854cda
Jake Kinsman <jkinsman1986@Jakes-MacBook-Pro.local>
2015-04-16 03:36:51 -0400

    thing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,9 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    puts "\n\n\n\n\n"
+    puts @user.inspect
+    puts "\n\n\n\n\n"
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
